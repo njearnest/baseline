bf91dd8cd9b7e15bdd5cc460e8f775b0762ad1cd
njearnest <48927552+njearnest@users.noreply.github.com>
2021-11-07 18:17:35 -0500

    Update removebloatware_c.ps1
    replaced curly quotes with real quotes

--- removebloatware_c.ps1
+++ removebloatware_c.ps1
@@ -1,8 +1,8 @@
-$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match “Microsoft Office 365 - en-us” }
+$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match "Microsoft Office 365 - en-us" }
 Uninstall-Package $app.name
 
-$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match “Microsoft Office 365 - es-es” }
+$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match "Microsoft Office 365 - es-es" }
 Uninstall-Package $app.name
 
-$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match “Microsoft Office 365 - fr-fr” }
+$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match "Microsoft Office 365 - fr-fr" }
 Uninstall-Package $app.name
