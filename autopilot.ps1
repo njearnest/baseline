aa8080091524b759971973e9e66fd6f24cd3f0df
njearnest <48927552+njearnest@users.noreply.github.com>
2021-11-23 11:50:57 -0500

    Gather Autopilot hash online directly into Intune.

---
+++ autopilot.ps1
@@ -0,0 +1,4 @@
+PowerShell.exe -ExecutionPolicy Bypass
+Install-Script -name Get-WindowsAutopilotInfo -Force
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Get-WindowsAutoPilotInfo -Online
