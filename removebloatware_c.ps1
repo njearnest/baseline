b0c8f4a54beed0341e99b6c813128c9476867ba6
njearnest <48927552+njearnest@users.noreply.github.com>
2022-05-30 20:00:15 -0400

    Delete removebloatware_c.ps1

--- removebloatware_c.ps1
+++
@@ -1,8 +0,0 @@
-$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match "Microsoft Office 365 - en-us" }
-Uninstall-Package $app.name
-
-$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match "Microsoft Office 365 - es-es" }
-Uninstall-Package $app.name
-
-$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match "Microsoft Office 365 - fr-fr" }
-Uninstall-Package $app.name
