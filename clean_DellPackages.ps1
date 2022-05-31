8751a78800c58226a9e7912b3da0d94d1b609d0a
njearnest <48927552+njearnest@users.noreply.github.com>
2022-05-30 20:20:32 -0400

    Added section to take care of older remnants of Dell SupportAssist

--- clean_DellPackages.ps1
+++ clean_DellPackages.ps1
@@ -2,3 +2,23 @@ get-package "*Dell SupportAssist*" | Uninstall-Package
 get-package "*Dell Command*" | Uninstall-Package
 get-package "*Dell Power*" | Uninstall-Package
 get-package "*Dell Digital*" | Uninstall-Package
+
+#REMOVE REMNANTS THAT ARE OLD SCHOOL PROGRAMS
+$DellPackages = Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where {$_.DisplayName -like "*Dell SupportAssist*"} 
+
+if ($DellPackages) 
+{ 
+    write-host "Starting Uninstallation..." -ForegroundColor Cyan 
+    foreach ($DellPackage in $DellPackages) 
+    { 
+        write-host "Removing $($DellPackage.DisplayName)" -ForegroundColor Yellow 
+        $UninstallString = $DellPackage.UninstallString 
+        $UninstallEXE = ($UninstallString -split '"')[1] 
+        $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False" 
+        Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait 
+    } 
+} 
+else 
+{ 
+    write-host "None found." -ForegroundColor Green 
+}
