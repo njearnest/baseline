0a837175bbfce808eefe45628a3717b5c2122ea8
njearnest <48927552+njearnest@users.noreply.github.com>
2022-05-30 20:02:47 -0400

    New format - cleanstart.ps1 calls specialized helpers
    clean_DellPackages.ps1 removes specific Dell Packages 
    clean_M365PreInstalled.ps1 removes OEM M365 installations (EN, FR, SP)

---
+++ clean_DellPackages.ps1
@@ -0,0 +1,4 @@
+get-package "*Dell SupportAssist*" | Uninstall-Package
+get-package "*Dell Command*" | Uninstall-Package
+get-package "*Dell Power*" | Uninstall-Package
+get-package "*Dell Digital*" | Uninstall-Package
---
+++ cleanstart.ps1
@@ -0,0 +1,5 @@
+# REMOVE DELL TRASH
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/njearnest/baseline/main/clean_DellPackages.ps1'))
+
+# REMOVE OEM M365 Installs
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/njearnest/baseline/main/clean_M365PreInstalled.ps1'))
