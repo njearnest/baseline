1934199a63b4f20900e7ae565c514e48d8b83c25
njearnest <48927552+njearnest@users.noreply.github.com>
2023-11-07 18:50:00 -0500

    Update cleanstart.ps1
    added line to remove OneNote

--- cleanstart.ps1
+++ cleanstart.ps1
@@ -1,5 +1,8 @@
 # REMOVE DELL TRASH
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/njearnest/baseline/main/clean_DellPackages.ps1'))
 
-# REMOVE OEM M365 Installs
+# REMOVE OEM M365 Office Installs
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/njearnest/baseline/main/clean_M365PreInstalled.ps1'))
+
+# REMOVE OEM M365 OneNote Installs
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/njearnest/baseline/main/clean_M365OneNotePreInstalled.ps1'))
